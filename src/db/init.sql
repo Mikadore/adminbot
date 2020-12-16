3521a88e32d26035317b8fb3e0c41a039ebb3a0f
Mikadore <e.meissner@seznam.cz>
2020-12-16 16:14:56 +0100

    tickets table

---
+++ src/db/init.sql
@@ -0,0 +1,17 @@
+/*
+    create tables for the DB to function
+*/
+
+USE discord;
+
+/*Servers are identified by their snowflake*/
+
+CREATE TABLE Tickets
+{
+    serverid        VARCHAR (32)    NOT NULL,   /*Snowflake*/
+    ticketid        VARCHAR (32)    NOT NULL,   /*Snowflake of the ticket's channel*/
+    open            BOOL ,          
+    creator         VARCHAR (32),               /*Snowflake*/
+    creation_date   DATETIME,       
+    assignee        VARCHAR (32),               /*Snowflake*/
+}
