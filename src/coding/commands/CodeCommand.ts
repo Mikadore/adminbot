415a7d1d72a6be1c87730c9c76df169b725b1999
Mikadore <e.meissner@seznam.cz>
2020-12-19 17:42:21 +0100

    --code languages

--- src/coding/commands/CodeCommand.ts
+++ src/coding/commands/CodeCommand.ts
@@ -1,20 +1,34 @@
 import {Command}                from './../../command';
 import {Client, Message}        from 'discord.js';
-import {languages}              from './LanguagesSub'
+import {showLanguages}          from './LanguagesSub'
 export class CodeCommand implements Command {
     public async run(command: string, msg: Message, bot: Client) : Promise<void>
     {
         let arr = command.split(' ');
-        switch(arr[0])
-        {
-            case 'languages': await languages(msg, bot); break;
+        try {
+            switch(arr[0])
+            {
+                case 'languages': { 
+                    await showLanguages(msg, bot); break;
+                }
+                case '': {
+                    await msg.channel.send('Invalid usage, use \`help code\` to see the usage of this command');
+                    break;
+                }
+                default: {
+                    await msg.channel.send(`<options> must be one of [languages]`).catch(console.error);
+                    break;
+                }
+            }
+        } catch {
+            await msg.channel.send('An error occured.').catch(console.error);
         }
     }
     public command = ['code', 'godbolt'];
     metadata = {
         name: 'Code',
         description: 'Uses https://godbolt.org to compile and run codeblocks',
-        usage: 'code <options> <codeblock>',
+        usage: 'code <options> <optional | codeblock>',
         module: 'Coding'
     }
 }
--- src/coding/commands/LanguagesSub.ts
+++ src/coding/commands/LanguagesSub.ts
@@ -1,12 +1,55 @@
-import { Message, Client }  from "discord.js";
-import { resolvePtr } from "dns";
-import 'node-fetch';
+import { Message, Client, MessageEmbed }    from "discord.js";
+import { embedPager }                       from './../../util/pages';
+const fetch = require('node-fetch');
 
-export async function languages(msg: Message, bot: Client)
+export async function fetchLanguages() : Promise<string[]>
 {
-    let response = await fetch("https://godbolt.org", {
+    let response = await fetch("https://godbolt.org/api/languages", {
         headers: [["Accept", "application/json"]]
     });
     let json = await response.json();
-    await msg.channel.send(JSON.stringify(json));
+
+    let languages: string[] = [];
+
+    for(let lang of json)
+    {
+        languages.push(lang['id']);
+    }
+    return languages;
+}
+export async function showLanguages(msg: Message, bot: Client) 
+{
+    let languages = await fetchLanguages();
+
+    let embeds: MessageEmbed[] = [];
+    
+    let string = '';
+    let counter  = 0;
+
+    for(let i = 0; i < languages.length; i++)
+    {
+        string += `**${i + 1})** ${languages[i]}\n`;
+        if(++counter >= 16)
+        {
+            embeds.push(
+                new MessageEmbed()
+                        .setFooter(`Requested by ${msg.author.tag}`, msg.author.displayAvatarURL())
+                        .addField(`Languages (${languages.length})`, string)
+            );
+            string = '';
+            counter = 0;
+        }
+    }
+    if(counter != 0)
+    {
+        embeds.push(
+            new MessageEmbed()
+                    .setFooter(`Requested by ${msg.author.tag}`, msg.author.displayAvatarURL())
+                    .addField(`Languages (${languages.length})`, string)
+        );
+        string = '';
+        counter = 0;
+    }
+    let sent = await msg.channel.send(embeds[0]);
+    await embedPager(sent, embeds, 0, 60);
 }
